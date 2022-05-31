0fd9bae49b83ad7d8cb7a9899dd783dda0a485eb
MariKutubidze <82153875+Marikutubidze@users.noreply.github.com>
2022-05-31 23:40:13 +0400

    add script.js

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ let overlayContent = document.getElementById("overlay");
 let closeOverlay = document.getElementById("close");
 let postContent = document.getElementById("content");
 
+let addButton = document.getElementById("add");
+let postOverlay = document.getElementById("post-overlay");
+let form = document.getElementById("form");
+let postButton = document.getElementById("add-post");
+
 // სერვერზე მოთხოვნის გაგზავნა
 
 function ajax(url, callback) {
@@ -38,16 +43,34 @@ function createPost(item) {
   let h3Tag = document.createElement("h3");
   h3Tag.innerText = item.title;
 
+  let deleteButton = document.createElement("button");
+  deleteButton.innerText = "delete post";
+  deleteButton.setAttribute("data-id", item.id);
+
   divWrapper.appendChild(h2Tag);
   divWrapper.appendChild(h3Tag);
+  divWrapper.appendChild(deleteButton);
+  mainWrapper.appendChild(divWrapper);
 
   divWrapper.addEventListener("click", function (event) {
     let id = event.target.getAttribute("data-id");
     openOverlay(id);
   });
 
-  mainWrapper.appendChild(divWrapper);
+  deleteButton.addEventListener("click", function (event) {
+    event.stopPropagation();
+    let id = event.target.getAttribute("data-id");
+    divWrapper.classList.add("deletepost");
+    deletePost(id);
+  });
+}
 
+function deletePost(id) {
+  let url = `https://jsonplaceholder.typicode.com/posts/${id}`;
+
+  fetch(url, function () {
+    method: "DELETE";
+  });
 }
 
 function openOverlay(id) {
@@ -73,4 +96,28 @@ function overlayFunction(item) {
 closeOverlay.addEventListener("click", function () {
   overlayContent.classList.remove("active");
   postContent.innerHTML = " ";
+});
+
+addButton.addEventListener("click", function () {
+  postOverlay.classList.add("overlay-active");
+});
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault;
+
+  let formData = {
+    title: event.target[0].value,
+    description: event.target[1].value,
+  };
+
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify(formData),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+  postOverlay.classList.remove("active-add");
 });
